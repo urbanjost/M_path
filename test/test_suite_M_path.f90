8bf196c25ccb11f34534a413c79a4dbfee6b3d1f
John S. Urban <urbanjost@comcast.net>
2023-04-21 13:54:42 -0400

    fpm 0.8.0

--- test/test_suite_M_path.f90
+++ test/test_suite_M_path.f90
@@ -1,9 +1,9 @@
 program runtest
-use M_msg
+use M_framework__msg
 use M_system,  only : system_getcwd
-use M_verify,  only : unit_check_start,unit_check,unit_check_done,unit_check_good,unit_check_bad,unit_check_msg
-use M_verify,  only : unit_check_level, unit_check_command, unit_check_keep_going
-use M_verify, only : unit_check_stop
+use M_framework__verify,  only : unit_check_start,unit_check,unit_check_done,unit_check_good,unit_check_bad,unit_check_msg
+use M_framework__verify,  only : unit_check_level, unit_check_command, unit_check_keep_going
+use M_framework__verify, only : unit_check_stop
 use M_path,    only : path
 implicit none
    unit_check_command=''
